fad3a4d6b55c17ce101dc88e551d37b9b0c612ba
Rhoda Jiao <r.jiao@utexas.edu>
2015-02-10 17:52:27 -0600

    stuff

--- 01 Data/birds.R
+++ 01 Data/birds.R
@@ -1,2 +1,3 @@
 birds <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from birds"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_rj6793', PASS='orcl_rj6793', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1)))) 
-head(birds)
+tbl_df(birds)
+
